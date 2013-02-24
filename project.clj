95887147c10d2002f5d2a4d1720305c070062f40
Luca Antiga <luca.antiga@orobix.com>
2013-02-24 23:34:31 +0100

    Added description and url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clj-toml "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "TOML for Clojure"
+  :url "https://github.com/lantiga/toml"
+  :author "Luca Antiga"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
