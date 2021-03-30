21ffa4937fb6d84e21c03d542bfece2438906205
Jinseop Kim <vmfhrmfoaj@gmail.com>
2021-03-30 23:59:11 +0900

    Uncomment a unit test that parse example-v0.4.0.toml
    and change the indentation

--- test/clj_toml/core_test.clj
+++ test/clj_toml/core_test.clj
@@ -183,123 +183,123 @@
 (deftest example-test
   (testing "TOML example"
     (is (= (parse-string (slurp "resources/example.toml"))
-            {"title" "TOML Example"
+           {"title" "TOML Example"
             "owner"
             {"name" "Tom Preston-Werner"
-              "organization" "GitHub"
-              "bio" "GitHub Cofounder & CEO\\nLikes tater tots and beer."
-              "dob" (read-instant-timestamp "1979-05-27T07:32:00Z")}
+             "organization" "GitHub"
+             "bio" "GitHub Cofounder & CEO\\nLikes tater tots and beer."
+             "dob" (read-instant-timestamp "1979-05-27T07:32:00Z")}
             "database"
             {"enabled" true,
-              "connection_max" 5000,
-              "server" "192.168.1.1",
-              "ports" [8001 8001 8002]}
+             "connection_max" 5000,
+             "server" "192.168.1.1",
+             "ports" [8001 8001 8002]}
             "servers"
             {"alpha"
-              {"ip" "10.0.0.1"
+             {"ip" "10.0.0.1"
               "dc" "eqdc10"}
-              "beta"
-              {"ip" "10.0.0.2"
+             "beta"
+             {"ip" "10.0.0.2"
               "dc" "eqdc10"
               "country" "中国"}}
             "clients"
             {"data" [["gamma" "delta"] [1 2]]
-              "hosts" ["alpha" "omega"]}
+             "hosts" ["alpha" "omega"]}
             "products"
             [{"name" "Hammer"
               "sku" 738594937}
-              {"name" "Nail"
+             {"name" "Nail"
               "sku" 284758393
               "color" "gray"}]}))))
 
 (deftest hard-example-test
   (testing "TOML hard example"
     (is (= (parse-string (slurp "resources/hard_example.toml"))
-            {"the"
+           {"the"
             {"hard"
-              {"another_test_string" " Same thing, but with a string #",
+             {"another_test_string" " Same thing, but with a string #",
               "test_array2"
               ["Test #11 ]proved that" "Experiment #9 was a success"],
               "test_array" ["] " " # "],
               "bit#"
               {"what?" "You don't think some user won't do that?",
-                "multi_line_array" ["]"]},
+               "multi_line_array" ["]"]},
               "harder_test_string"
               " And when \\\"'s are in the string, along with # \\\""},
-              "test_string" "You'll hate me after this - #"}}))))
+             "test_string" "You'll hate me after this - #"}}))))
 
-#_(deftest example-v0.4.0-test
-    (testing "TOML 0.4.0 example"
-      (is (= (parse-string (slurp "resources/example-v0.4.0.toml"))
-             {"table"
-              {"key" "value"
-               "subtable" {"key" "another value"}
-               "inline" {"first" "Tom" "last" "Preston-Werner"}}
-              "x" {"y" {"z" {"w" {}}}}
-              "string"
-              {"basic"
-               {"basic" "I'm a string. \"You can quote me\". Name\tJos\u00E9\nLocation\tSF."}
-               "multiline"
-               {"key1" "One\nTwo"
-                "key2" "One\nTwo"
-                "key3" "One\nTwo"
-                "continued"
-                {"key1" "The quick brown fox jumps over the lazy dog."
-                 "key2" "The quick brown fox jumps over the lazy dog."
-                 "key3" "The quick brown fox jumps over the lazy dog."}}
-               "literal"
-               {"winpath" "C:\\Users\\nodejs\\templates"
-                "winpath2" "\\\\ServerX\\admin$\\system32\\"
-                "quoted" "Tom \"Dubs\" Preston-Werner"
-                "regex" "<\\i\\c*\\s*>"
-                "multiline"
-                {"regex2" "I [dw]on't need \\d{2} apples"
-                 "lines" "The first newline is\ntrimmed in raw strings.\n   All other whitespace\n   is preserved.\n"}}}
-              "integer"
-              {"key1" 99
-               "key2" 42
-               "key3" 0
-               "key4" -17
-               "underscores"
-               {"key1" 1000
-                "key2" 5349221
-                "key3" 12345}}
-              "float"
-              {"fractional"
-               {"key1" 1.0
-                "key2" 3.1415
-                "key3" -0.01}
-               "exponent"
-               {"key1" 5e+22
-                "key2" 1e6
-                "key3" -2E-2}
-               "both"
-               {"key" 6.626e-34}
-               "underscores"
-               {"key1" 9224617.445991228313
-                "key2" 1e1000}}
-              "boolean"
-              {"True" true
-               "False" false}
-              "datetime"
-              {"key1" "1979-05-27T07:32:00Z"
-               "key2" "1979-05-27T00:32:00-07:00"
-               "key3" "1979-05-27T00:32:00.999999-07:00"}
-              "array"
-              {"key1" [1 2 3]
-               "key2" ["red" "yellow" "green"]
-               "key3" [[1 2] [3 4 5]]
-               "key4" [[1 2] ["a" "b" "c"]]
-               "key5" [1 2 3]
-               "key6" [1 2]}
-              "products"
-              [{"name" "Hammer" "sku" 738594937}
-               {}
-               {"name" "Neil" "sku" 284758393 "color" "gray"}]
-              "fruit"
-              [{"name" "apple"
-                "physical" {"color" "red" "shape" "round"}
-                "variety" [{"name" "red delicious"}
-                           {"name" "granny smith"}]}
-               {"name" "banana"
-                "variety" [{"name" "plantain"}]}]}))))
+(deftest example-v0.4.0-test
+  (testing "TOML 0.4.0 example"
+    (is (= (parse-string (slurp "resources/example-v0.4.0.toml"))
+           {"table"
+            {"key" "value"
+             "subtable" {"key" "another value"}
+             "inline" {"first" "Tom" "last" "Preston-Werner"}}
+            "x" {"y" {"z" {"w" {}}}}
+            "string"
+            {"basic"
+             {"basic" "I'm a string. \"You can quote me\". Name\tJos\u00E9\nLocation\tSF."}
+             "multiline"
+             {"key1" "One\nTwo"
+              "key2" "One\nTwo"
+              "key3" "One\nTwo"
+              "continued"
+              {"key1" "The quick brown fox jumps over the lazy dog."
+               "key2" "The quick brown fox jumps over the lazy dog."
+               "key3" "The quick brown fox jumps over the lazy dog."}}
+             "literal"
+             {"winpath" "C:\\Users\\nodejs\\templates"
+              "winpath2" "\\\\ServerX\\admin$\\system32\\"
+              "quoted" "Tom \"Dubs\" Preston-Werner"
+              "regex" "<\\i\\c*\\s*>"
+              "multiline"
+              {"regex2" "I [dw]on't need \\d{2} apples"
+               "lines" "The first newline is\ntrimmed in raw strings.\n   All other whitespace\n   is preserved.\n"}}}
+            "integer"
+            {"key1" 99
+             "key2" 42
+             "key3" 0
+             "key4" -17
+             "underscores"
+             {"key1" 1000
+              "key2" 5349221
+              "key3" 12345}}
+            "float"
+            {"fractional"
+             {"key1" 1.0
+              "key2" 3.1415
+              "key3" -0.01}
+             "exponent"
+             {"key1" 5e+22
+              "key2" 1e6
+              "key3" -2E-2}
+             "both"
+             {"key" 6.626e-34}
+             "underscores"
+             {"key1" 9224617.445991228313
+              "key2" 1e1000}}
+            "boolean"
+            {"True" true
+             "False" false}
+            "datetime"
+            {"key1" "1979-05-27T07:32:00Z"
+             "key2" "1979-05-27T00:32:00-07:00"
+             "key3" "1979-05-27T00:32:00.999999-07:00"}
+            "array"
+            {"key1" [1 2 3]
+             "key2" ["red" "yellow" "green"]
+             "key3" [[1 2] [3 4 5]]
+             "key4" [[1 2] ["a" "b" "c"]]
+             "key5" [1 2 3]
+             "key6" [1 2]}
+            "products"
+            [{"name" "Hammer" "sku" 738594937}
+             {}
+             {"name" "Neil" "sku" 284758393 "color" "gray"}]
+            "fruit"
+            [{"name" "apple"
+              "physical" {"color" "red" "shape" "round"}
+              "variety" [{"name" "red delicious"}
+                         {"name" "granny smith"}]}
+             {"name" "banana"
+              "variety" [{"name" "plantain"}]}]}))))
